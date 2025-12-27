2078d26ecd8a4170efaca1659b554563e54d03a4
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2025-12-27 15:45:05 -0800

    Create 122-BestTimeToBuyAndSellStockIi

---
+++ 122-BestTimeToBuyAndSellStockIi/122-BestTimeToBuyAndSellStockIi.cpp
@@ -0,0 +1,16 @@
+// Last updated: 12/27/2025, 3:45:03 PM
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int maxProf = 0;
+
+
+        for(int i = 1; i < prices.size(); ++i) {
+            if(prices[i] > prices[i-1]) maxProf+= prices[i] - prices[i-1];
+        }
+
+        return maxProf;
+    }
+
+};
