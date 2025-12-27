9c8b6c02a4e7127d8c058975d24b761d78eaceac
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2025-12-27 15:44:47 -0800

    Create 626-ExchangeSeats

---
+++ 626-ExchangeSeats/626-ExchangeSeats.sql
@@ -0,0 +1,11 @@
+-- Last updated: 12/27/2025, 3:44:44 PM
+SELECT
+CASE
+    WHEN id%2=1 AND id=(select max(id) from Seat) THEN id
+    WHEN id%2=0 THEN id-1
+    WHEN id%2=1 THEN id+1
+END
+AS id, student
+FROM Seat
+ORDER BY id
+;
