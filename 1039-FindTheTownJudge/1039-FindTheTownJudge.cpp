817d03a4c0b3704b06f47f9ac105ac36b99b647f
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2025-12-27 15:44:35 -0800

    Create 1039-FindTheTownJudge

---
+++ 1039-FindTheTownJudge/1039-FindTheTownJudge.cpp
@@ -0,0 +1,20 @@
+// Last updated: 12/27/2025, 3:44:32 PM
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> in(n);
+        vector<int> out(n);
+
+        for(auto & i : trust) {
+            ++in[i[1]-1];
+            ++out[i[0]-1];
+        }
+
+        for(int i = 0; i < n; ++i) {
+            if(in[i] == n-1 && out[i] == 0) return i+1;
+        }
+
+        return -1;
+
+    }
+};
