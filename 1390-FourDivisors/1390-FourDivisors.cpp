2930fd510cab6650c78bbb23a33e2d44e802cd91
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2026-01-03 16:23:13 -0800

    File updated at 1/3/2026, 4:23:13 PM

--- 1390-FourDivisors/1390-FourDivisors.cpp
+++ 1390-FourDivisors/1390-FourDivisors.cpp
@@ -1,4 +1,4 @@
-// Last updated: 1/3/2026, 4:22:24 PM
+// Last updated: 1/3/2026, 4:23:13 PM
 1class Solution {
 2public:
 3    int sumFourDivisors(vector<int>& nums) {
@@ -17,21 +17,20 @@
 16
 17            int numDivisors = 2;
 18            int divisorSum = 1 + i;
-19            // don't want square root, odd number of divisors
-20            for(int j = 2; j * j <= i; ++j) {
-21                if(i % j == 0) {
-22                    if(j * j == i) numDivisors++;
-23                    else {
-24                        numDivisors += 2;
-25                        if(numDivisors == 6) break;
-26                        divisorSum += j + (i / j); 
-27                    }  
-28                }
-29            }
-30            if(numDivisors == 4) { map[i] = divisorSum; total += divisorSum; }
-31            else map[i] = -1;
-32        }
-33
-34        return total;
-35    }
-36};
+19            for(int j = 2; j * j <= i; ++j) {
+20                if(i % j == 0) {
+21                    if(j * j == i) numDivisors++;
+22                    else {
+23                        numDivisors += 2;
+24                        if(numDivisors == 6) break;
+25                        divisorSum += j + (i / j); 
+26                    }  
+27                }
+28            }
+29            if(numDivisors == 4) { map[i] = divisorSum; total += divisorSum; }
+30            else map[i] = -1;
+31        }
+32
+33        return total;
+34    }
+35};
