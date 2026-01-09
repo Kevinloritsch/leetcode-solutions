e109d1b1a53bc38c3852f113318de276669a674a
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2026-01-09 11:27:48 -0800

    File updated at 1/9/2026, 11:27:47 AM

--- 865-SmallestSubtreewithalltheDeepestNodes/865-SmallestSubtreewithalltheDeepestNodes.cpp
+++ 865-SmallestSubtreewithalltheDeepestNodes/865-SmallestSubtreewithalltheDeepestNodes.cpp
@@ -1,4 +1,4 @@
-// Last updated: 1/9/2026, 11:22:50 AM
+// Last updated: 1/9/2026, 11:27:47 AM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -13,11 +13,19 @@
 12class Solution {
 13public:
 14    int depth(TreeNode *root) {
-15        return !root ? 0 : max(depth(root->left), depth(root->right)) + 1;
-16    }
-17
-18    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-19        int d = depth(root->left) - depth(root->right);
-20        return d == 0 ? root : subtreeWithAllDeepest(d > 0 ? root->left : root->right);
-21    }
-22};
+15        if(!root) return 0;
+16        return max(depth(root->left), depth(root->right)) + 1;
+17    }
+18
+19    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+20        int depthCheck = depth(root->left) - depth(root->right);
+21        // balanced, need them all for lca
+22        if(depthCheck == 0) return root;
+23
+24        // left heavy 
+25        else if (depthCheck > 0) return subtreeWithAllDeepest(root -> left);
+26
+27        // right heavy
+28        else return subtreeWithAllDeepest(root -> right);
+29    }
+30};
