681f4792010659122dc7fdba1b8a8c3f9af2fbdb
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2026-01-22 14:49:04 -0800

    File updated at 1/22/2026, 2:49:03 PM

--- 207-CourseSchedule/207-CourseSchedule.cpp
+++ 207-CourseSchedule/207-CourseSchedule.cpp
@@ -1,45 +1,33 @@
-// Last updated: 12/27/2025, 3:44:54 PM
-class Solution {
-public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
-        vector<pair<list<int>, int>> adjacencyList(numCourses, {list<int>(), -1});
-
-        for(auto& i : prerequisites) {
-            adjacencyList[i[0]].first.push_back(i[1]);
-        }
-
-        for(int i = 0; i < numCourses; ++i) {
-            // technically have to check every spot (dependency can be anywhere)
-            if (adjacencyList[i].second == -1)  if (!dfs(adjacencyList, i)) return false;
-
-        }
-        
-        return true;
-    }
-
-    bool dfs(vector<pair<list<int>, int>>& adjacencyList, int visited) {
-        
-        // memoize yay
-        if(adjacencyList[visited].second == 2) return true;
-
-        // we visited this spot this iteration
-        adjacencyList[visited].second = 0;
-
-        // dfs
-        for(auto& j : adjacencyList[visited].first) {
-            if(adjacencyList[j].second == 0) return false;
-            if(!dfs(adjacencyList, j)) return false;
-        }
-
-        adjacencyList[visited].second = 2;
-
-        return true;
-    }
-
-    /*
-        generate a graph (adjacency list) by iterating over prereqs
-        iterate through it, doing a dfs, at any point if we find a cycle its over and return false
-        if iterate through everything, its allg!
-    */
-};
+// Last updated: 1/22/2026, 2:49:03 PM
+1class Solution {
+2public:
+3    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+4
+5        vector<vector<int>> adjacencyList(numCourses);
+6
+7        for(auto& i : prerequisites) adjacencyList[i[0]].push_back(i[1]);
+8
+9        vector<int> visited(numCourses, -1);
+10
+11        for(int i = 0; i < numCourses; ++i) {
+12            if (visited[i] == -1)  if (!dfs(adjacencyList, visited, i)) return false;
+13        }
+14        
+15        return true;
+16    }
+17
+18    bool dfs(vector<vector<int>>& adjacencyList, vector<int>& visited, int next) {
+19        if(visited[next] == 1) return true;
+20
+21        visited[next] = 0;
+22
+23        for(auto& j : adjacencyList[next]) {
+24            if(visited[j] == 0) return false;
+25            if(!dfs(adjacencyList, visited, j)) return false;
+26        }
+27
+28        visited[next] = 1;
+29
+30        return true;
+31    }
+32};
