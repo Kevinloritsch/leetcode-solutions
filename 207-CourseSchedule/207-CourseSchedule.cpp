750ec985a5aefe903d12a8da94bc7e6e4121272c
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2026-01-22 17:10:36 -0800

    File updated at 1/22/2026, 5:10:36 PM

--- 207-CourseSchedule/207-CourseSchedule.cpp
+++ 207-CourseSchedule/207-CourseSchedule.cpp
@@ -1,33 +1,31 @@
-// Last updated: 1/22/2026, 2:49:03 PM
+// Last updated: 1/22/2026, 5:10:36 PM
 1class Solution {
 2public:
 3    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
 4
-5        vector<vector<int>> adjacencyList(numCourses);
-6
-7        for(auto& i : prerequisites) adjacencyList[i[0]].push_back(i[1]);
-8
-9        vector<int> visited(numCourses, -1);
-10
-11        for(int i = 0; i < numCourses; ++i) {
-12            if (visited[i] == -1)  if (!dfs(adjacencyList, visited, i)) return false;
-13        }
-14        
-15        return true;
-16    }
-17
-18    bool dfs(vector<vector<int>>& adjacencyList, vector<int>& visited, int next) {
-19        if(visited[next] == 1) return true;
-20
-21        visited[next] = 0;
-22
-23        for(auto& j : adjacencyList[next]) {
-24            if(visited[j] == 0) return false;
-25            if(!dfs(adjacencyList, visited, j)) return false;
-26        }
-27
-28        visited[next] = 1;
-29
-30        return true;
-31    }
-32};
+5        vector<vector<int>> adjList(numCourses);
+6        for(auto i : prerequisites) adjList[i[0]].push_back(i[1]);
+7
+8        vector<int> visited(numCourses, -1);
+9
+10        for(int i = 0; i < adjList.size(); ++i) {
+11            if(visited[i] == -1) if(!dfs(adjList, visited, i)) return false;
+12        }
+13
+14        return true;
+15    }
+16
+17    bool dfs(vector<vector<int>>& adjList, vector<int>& visited, int next) {
+18        if(visited[next] == 1) return true;
+19
+20        visited[next] = 0;
+21
+22        for(auto i : adjList[next]) {
+23            if(visited[i] == 0) return false;
+24            if(!dfs(adjList, visited, i)) return false;
+25        }
+26
+27        visited[next] = 1;
+28        return true;
+29    }
+30};
