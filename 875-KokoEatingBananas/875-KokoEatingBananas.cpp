f2805b1e0891eb6a13263bcefa4b57351a0c977e
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2026-01-29 17:06:36 -0800

    File updated at 1/29/2026, 5:06:35 PM

--- 875-KokoEatingBananas/875-KokoEatingBananas.cpp
+++ 875-KokoEatingBananas/875-KokoEatingBananas.cpp
@@ -1,30 +1,32 @@
-// Last updated: 1/29/2026, 4:48:40 PM
+// Last updated: 1/29/2026, 5:06:35 PM
 1class Solution {
 2public:
 3    int minEatingSpeed(vector<int>& piles, int h) {
 4
 5        int low = 1;
 6        int high = INT_MAX / 2;
-7        int candidate = high;
-8
-9        while (low <= high) {
-10            int mid = low + (high - low) / 2;
-11
-12            long hoursLeft = h;
-13            for (int i : piles) {
-14                if (i <= mid) hoursLeft -= 1;
-15                else if (i % mid == 0) hoursLeft -= i / mid;
-16                else hoursLeft -= (i / mid) + 1;
-17            }
-18
-19            if (hoursLeft < 0) low = mid + 1;
-20            else {
-21                candidate = mid;
-22                high = mid - 1;
-23            }
-24        }
-25
-26        return candidate;
-27    }
-28};
-29
+7        int candidate = INT_MAX;
+8        int mid;
+9
+10        while(high >= low) {
+11            mid = low + (high - low) / 2;
+12
+13            int r = h;
+14
+15            for(auto i : piles) {
+16                if(i <= mid) --r;
+17                else if (i % mid == 0) r -= (i / mid);
+18                else r -= (i / mid) + 1;
+19            }
+20
+21            if(r < 0) low = mid + 1;
+22            else {
+23                candidate = min(candidate, mid);
+24                high = mid - 1;
+25            }
+26        }
+27
+28        return candidate;
+29    }
+30};
+31
