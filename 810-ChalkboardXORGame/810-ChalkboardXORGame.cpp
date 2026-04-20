88ae6c4dd9c58db63c0cbac97c74b9b2c8111c82
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2026-04-20 13:33:51 -0700

    File updated at 4/20/2026, 1:33:49 PM

--- 810-ChalkboardXORGame/810-ChalkboardXORGame.cpp
+++ 810-ChalkboardXORGame/810-ChalkboardXORGame.cpp
@@ -1,31 +1,30 @@
-// Last updated: 4/20/2026, 1:33:42 PM
+// Last updated: 4/20/2026, 1:33:49 PM
 1class Solution {
 2public:
 3    bool xorGame(vector<int>& nums) {
 4        int xorCount = 0;
 5        for(auto i : nums) xorCount = xorCount ^ i;
 6        if(xorCount == 0) return true;
-7        cout << xorCount;
-8
-9        return 1 - nums.size() % 2;
-10    }
-11};
+7
+8        return 1 - nums.size() % 2;
+9    }
+10};
+11
 12
-13
-14/*
-15win by default
-161, 2, 3
-171, 10, 11
-182, 2
-190
-20
+13/*
+14win by default
+151, 2, 3
+161, 10, 11
+172, 2
+180
+19
+200, 1
 210, 1
-220, 1
-231
-24
-251, 1, 2
-261, 1, 10
-271, 2
+221
+23
+241, 1, 2
+251, 1, 10
+261, 2
+27
 28
-29
-30*/
+29*/
