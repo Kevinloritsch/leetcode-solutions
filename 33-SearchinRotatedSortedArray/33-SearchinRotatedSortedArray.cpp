8c2dc40acfe3191710f313616cdc8aecd485af3d
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2026-05-22 11:43:24 -0700

    File updated at 5/22/2026, 11:43:23 AM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/22/2026, 11:43:07 AM
+// Last updated: 5/22/2026, 11:43:23 AM
 1class Solution {
 2public:
 3    int search(vector<int>& nums, int target) {
@@ -10,22 +10,20 @@
 9
 10        while(low <= high) {
 11            int mid = low + (high - low) / 2;
-12            cout << mid << endl;
+12            if(nums[mid] == target) return mid;
 13
-14            if(nums[mid] == target) return mid;
-15
-16            if(nums[low] <= nums[mid]) {
-17                if(nums[low] <= target && nums[mid] >= target) high = mid -1;
-18                else low = mid + 1;
-19            }
-20            else {
-21                if(nums[high] >= target && nums[mid] <= target) low = mid + 1;
-22                else high = mid - 1;
-23            }
-24        }
-25
-26        return -1;
-27        
-28    }
-29
-30};
+14            if(nums[low] <= nums[mid]) {
+15                if(nums[low] <= target && nums[mid] >= target) high = mid -1;
+16                else low = mid + 1;
+17            }
+18            else {
+19                if(nums[high] >= target && nums[mid] <= target) low = mid + 1;
+20                else high = mid - 1;
+21            }
+22        }
+23
+24        return -1;
+25        
+26    }
+27
+28};
