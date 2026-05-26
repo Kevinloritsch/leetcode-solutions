63ef6b30fcbad9537c44cae2c2346ca24e5780bc
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2026-05-26 14:01:07 -0700

    Create 2178-MaximumSplitofPositiveEvenIntegers

---
+++ 2178-MaximumSplitofPositiveEvenIntegers/2178-MaximumSplitofPositiveEvenIntegers.cpp
@@ -0,0 +1,25 @@
+// Last updated: 5/26/2026, 2:01:04 PM
+1class Solution {
+2public:
+3    vector<long long> maximumEvenSplit(long long finalSum) {
+4        vector<long long> ans;
+5        if(finalSum % 2 == 1) return ans;
+6
+7        int guess = 2;
+8        while(finalSum > 0){
+9            ans.push_back(guess);
+10            finalSum -= guess;
+11            guess += 2;
+12        }
+13
+14        if(finalSum == 0) return ans;
+15
+16        finalSum *= -1;
+17        for(int i = 0; i < ans.size(); ++i) if(ans[i] == finalSum)
+18            ans.erase(std::remove(ans.begin(), ans.end(), finalSum), ans.end());
+19
+20
+21        return ans;
+22        
+23    }
+24};
