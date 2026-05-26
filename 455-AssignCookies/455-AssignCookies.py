f6ed4fe237caccd9a0ed7ac7d68ec4a0037dffa2
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2026-05-26 13:38:26 -0700

    File updated at 5/26/2026, 1:38:24 PM

--- 455-AssignCookies/455-AssignCookies.py
+++ 455-AssignCookies/455-AssignCookies.py
@@ -1,14 +1,19 @@
-# Last updated: 5/26/2026, 1:24:04 PM
+# Last updated: 5/26/2026, 1:38:24 PM
 1class Solution:
-2    def findContentChildren(self, g: List[int], s: List[int]) -> int:
-3        # smallest greed factor get satisfied first 
-4        # sort s and g in increasing order 
-5        g.sort()
-6        s.sort()
-7        l = 0
-8        m = 0
-9        while l < len(g) and m < len(s):
-10            if s[m] >= g[l]:
-11                l += 1
-12            m += 1
-13        return l
+2    def isPalindrome(self, x: int) -> bool:
+3        # left and right pointer 
+4        # when left = right and so far, word has been equal, then length is odd and word is palindrome
+5        # if length is even, left will never = right
+6        strx = str(x)
+7
+8        left = 0 
+9        right = len(strx)-1
+10        while left <= right:
+11            if strx[left] != strx[right]:
+12                return False
+13            left += 1
+14            right -= 1
+15        
+16        return True
+17
+18        
