d7820e811e0a1f3e166a2a4daf4547974ed867dc
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2026-05-28 19:49:37 -0700

    Create 3300-MinimumElementAfterReplacementWithDigitSum

---
+++ 3300-MinimumElementAfterReplacementWithDigitSum/3300-MinimumElementAfterReplacementWithDigitSum.cpp
@@ -0,0 +1,15 @@
+// Last updated: 5/28/2026, 7:49:36 PM
+1class Solution {
+2public:
+3    int minElement(vector<int>& nums) {
+4        int smallest = INT_MAX / 2;
+5        int curr = 0;
+6        for(auto i : nums) {
+7            while(i > 0) { curr += i % 10; i /= 10; }
+8            if (curr < smallest) smallest = curr;
+9            curr = 0;
+10        }
+11
+12        return smallest;        
+13    }
+14};
