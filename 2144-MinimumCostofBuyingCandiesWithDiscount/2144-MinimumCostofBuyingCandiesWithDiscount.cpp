8fbb8ed5247f117020ec478aa447df67b09ea446
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2026-06-01 10:53:10 -0700

    Create 2144-MinimumCostofBuyingCandiesWithDiscount

---
+++ 2144-MinimumCostofBuyingCandiesWithDiscount/2144-MinimumCostofBuyingCandiesWithDiscount.cpp
@@ -0,0 +1,21 @@
+// Last updated: 6/1/2026, 10:53:10 AM
+1class Solution {
+2public:
+3    int minimumCost(vector<int>& cost) {
+4        std::sort(cost.begin(), cost.end());
+5
+6        int sum = 0;
+7        int num = 1;
+8
+9        for(int i = cost.size() - 1; i >= 0; --i) {
+10            if(num == 3) {
+11                num = 1;
+12                continue;
+13            }
+14            sum += cost[i];
+15            num++;
+16        }  
+17        return sum;
+18        
+19    }
+20};
