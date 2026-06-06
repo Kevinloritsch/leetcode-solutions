cfd0eb585d46562061a6e0fa70f9886b7feb819a
Kevin Loritsch <56408094+Kevinloritsch@users.noreply.github.com>
2026-06-06 11:01:56 -0700

    File updated at 6/6/2026, 11:01:55 AM

--- 2574-LeftandRightSumDifferences/2574-LeftandRightSumDifferences.cpp
+++ 2574-LeftandRightSumDifferences/2574-LeftandRightSumDifferences.cpp
@@ -1,4 +1,4 @@
-// Last updated: 6/6/2026, 11:01:51 AM
+// Last updated: 6/6/2026, 11:01:55 AM
 1class Solution {
 2public:
 3    vector<int> leftRightDifference(vector<int>& nums) {
@@ -7,11 +7,10 @@
 6        vector<int> ans;
 7        int right = 0;
 8        for(int i : nums) {
-9            cout << i << endl;
-10            sum -= i;
-11            ans.push_back(abs(right - sum));
-12            right += i;
-13        }
-14        return ans;        
-15    }
-16};
+9            sum -= i;
+10            ans.push_back(abs(right - sum));
+11            right += i;
+12        }
+13        return ans;        
+14    }
+15};
